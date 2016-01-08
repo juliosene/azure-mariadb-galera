58b3bb1ccd8ad692800f0dcb9562ecd73abbf62f
Julio Sene França <julio.sene@gmail.com>
2016-01-08 19:31:00 -0200

    Update mariadb-galera-install.sh

--- mariadb-galera-install.sh
+++ mariadb-galera-install.sh
@@ -1,33 +1,64 @@
 #!/bin/bash
 # Install MariaDB Galera Cluster
+#
+# $1 - number of nodes; $2 - cluster name;
+#
+NNODES=${1-1}
+PASSWORD=${2:-`date +%D%A%B | md5sum| sha256sum | base64| fold -w16| head -n1`}
+IPLIST=`echo ""`
+MYIP=`ip route get 10.0.0.5 | awk 'NR==1 {print $NF}'`
+MYNAME=`echo "Node$MYIP" | sed 's/10.0.0./-/'`
+CNAME=${3:-"GaleraCluster"}
+
+for (( n=1; n<=$NNODES; n++ ))
+do
+   IPLIST+=`echo "10.0.0.$n"`
+   if [ "$n" -lt $NNODES ];
+   then
+        IPLIST+=`echo ","`
+   fi
+done
+
 cd ~
-# apt-get update
-# apt-get -fy dist-upgrade
-# apt-get -fy upgrade
+#apt-get update
+#apt-get -fy dist-upgrade
+#apt-get -fy upgrade
 apt-get install lsb-release bc
 REL=`lsb_release -sc`
 DISTRO=`lsb_release -is | tr [:upper:] [:lower:]`
 # NCORES=` cat /proc/cpuinfo | grep cores | wc -l`
 # WORKER=`bc -l <<< "4*$NCORES"`
 
-apt-get install python-software-properties
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
-add-apt-repository "deb http://mirror.edatel.net.co/mariadb/repo/5.5/$DISTRO $REL main"
+apt-get install -y python-software-properties python-software-properties-common
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+add-apt-repository "deb http://mirror.edatel.net.co/mariadb/repo/10.1/$DISTRO $REL main"
 
 apt-get update
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y rsync galera mariadb-galera-server
 
-echo -e '[mysqld]
-#mysql settings
-binlog_format=ROW
-default-storage-engine=innodb
-innodb_autoinc_lock_mode=2
-query_cache_size=0
-query_cache_type=0
-bind-address=0.0.0.0
-#galera settings
-wsrep_provider=/usr/lib/galera/libgalera_smm.so
-wsrep_cluster_name="my_wsrep_cluster"
-# wsrep_cluster_address="gcomm://node1_ip,node2_ip,node3_ip"
-wsrep_sst_method=rsync' > /etc/mysql/conf.d/cluster.cnf
+DEBIAN_FRONTEND=noninteractive apt-get install -y rsync mariadb-server galera-3
+
+service mysql stop
+
+# create Galera config file
+
+wget https://raw.githubusercontent.com/juliosene/azure-mariadb-galera/master/cluster.cnf
+
+sed -i "s/IPLIST/$IPLIST/g;s/MYIP/$MYIP/g;s/MYNAME/$MYNAME/g;s/CLUSTERNAME/$CNAME/g" cluster.cnf
+mv cluster.cnf /etc/mysql/conf.d/cluster.cnf
+
+# Create Debian manager config file
+
+wget https://raw.githubusercontent.com/juliosene/azure-mariadb-galera/master/debian.cnf
+
+sed -i "s/IPLIST/$IPLIST/g;s/MYIP/$MYIP/g;s/MYNAME/$MYNAME/g;s/#PASSWORD#/$PASSWORD/g" debian.cnf
+mv debian.cnf /etc/mysql/debian.cnf
+
+# Starts a cluster if is the first node
+
+if [ "10.0.0.1" = "$MYIP" ];
+then
+   service mysql start --wsrep-new-cluster
+else
+   service mysql start
+fi
